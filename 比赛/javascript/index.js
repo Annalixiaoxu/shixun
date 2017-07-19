d612731fcb4a6ed78f977c8e67215272245f5713
Annalixiaoxu <2465194740@qq.com>
2017-07-19 13:31:48 +0800

    比赛网页zuizhong

--- 比赛/javascript/index.js
+++ 比赛/javascript/index.js
@@ -19,6 +19,62 @@ $("#competition-rules").on("click", function(){
      }, 500);
 
  });
+var oBanner = document.getElementById('banner');
+var oTab = document.getElementById('tab');
+var aLi = oTab.getElementsByTagName('li');
+var oContent = document.getElementById('content');
+var aImg = oContent.getElementsByTagName('img');
+var oPrev = document.getElementById('prev');
+var oNext = document.getElementById('next');
+
+var iNow = 0;
+
+for(var i=0; i<aLi.length; i++){
+    aLi[i].index = i;
+    aLi[i].onmouseover = function(){
+        changeImg(this.index);
+    };
+}
+
+oPrev.onclick = function(){
+    iNow--;
+    if(iNow == -1){
+        iNow = aLi.length - 1;
+    }
+    changeImg(iNow);
+};
+oNext.onclick = function(){
+    iNow++;
+    if(iNow == aLi.length){
+        iNow = 0;
+    }
+    changeImg(iNow);
+};
+
+function changeImg(idx){
+    for(var i=0; i<aLi.length; i++){
+        aLi[i].className = '';
+        aImg[i].className = '';
+    }
+    aLi[idx].className = 'selected';
+    aImg[idx].className = 'selected';
+}
+
+var timer;
+function run(){
+    timer = setInterval(function(){
+        oNext.onclick();
+    }, 2000);
+}
+run();
+
+oBanner.onmouseover = function(){
+    clearInterval(timer);
+};
+oBanner.onmouseout = function(){
+    run();
+};
+
 $("#go-up").on("click", function(){
      if($(window).scrollTop() <= 1){
          return false;
